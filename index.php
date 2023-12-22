779a9f9c45549b44e422dc33e434b7f0682b27ab
Huzum Andreea <92792751+AndreeaHm@users.noreply.github.com>
2023-12-22 17:20:06 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+    <title>Login</title>
+</head>
+<body>
+    <h2>Login</h2>
+
+    <form method="post" action="">
+        <label for="email">Email:</label>
+        <input type="text" id="email" name="email" required>
+
+        <label for="parola">Parola:</label>
+        <input type="password" id="parola" name="parola" required>
+
+        <button type="submit">Login</button>
+        <div class="g-recaptcha" data-sitekey="6Le3PTcpAAAAAJCWfgcBCmsdbXsDevvpXxqtj4WG"></div>
+    </form>
+
+    <p>Don't have an account? <a href="signup.php">Sign up here</a>.</p>
+
+<?php
+session_start();
+
+include 'db.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (isset($_POST['email']) && isset($_POST['parola'])) {
+        $email = htmlspecialchars($_POST['email'], ENT_QUOTES, 'UTF-8');
+        $parola = htmlspecialchars($_POST['parola'], ENT_QUOTES, 'UTF-8');
+
+        $recaptchaSecretKey = '6Le3PTcpAAAAANsqMsO4OPChHBVWh81R342p0fGK';
+        $recaptchaResponse = $_POST['g-recaptcha-response'];
+
+        $recaptchaUrl = "https://www.google.com/recaptcha/api/siteverify";
+        $recaptchaData = [
+            'secret' => $recaptchaSecretKey,
+            'response' => $recaptchaResponse,
+        ];
+
+        $recaptchaOptions = [
+            'http' => [
+                'method' => 'POST',
+                'content' => http_build_query($recaptchaData),
+            ],
+        ];
+
+        $recaptchaContext = stream_context_create($recaptchaOptions);
+        $recaptchaResult = file_get_contents($recaptchaUrl, false, $recaptchaContext);
+        $recaptchaResult = json_decode($recaptchaResult, true);
+
+        if (!$recaptchaResult['success']) {
+            echo '<script>alert("Eroare verificare reCAPTCHA.");</script>';
+            exit();
+        }
+
+        $stmt = $conn->prepare("SELECT * FROM utilizatori WHERE email = ? AND parola = ?");
+        $stmt->bind_param("ss", $email, $parola);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        if ($result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+
+            $userRole = $row['rol_user'];
+            $_SESSION['user_role'] = $userRole;
+
+            if ($userRole == 'utilizator') {
+                header('Location: utilizator.php');
+                exit();
+            } elseif ($userRole == 'bibliotecar') {
+                header('Location: bibliotecar.php');
+                exit();
+            } elseif ($userRole == 'admin') {
+                header('Location: admin.php');
+                exit();
+            } else {
+                echo '<script>alert("Rol necunoscut!");</script>';
+            }
+        } else {
+            echo '<script>alert("Date incorecte/insuficiente.");</script>';
+        }
+    }
+}
+
+session_regenerate_id(true);
+session_unset();
+session_destroy();
+?>
+</body>
+</html>
